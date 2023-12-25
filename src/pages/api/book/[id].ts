7eaff8f345e09ea73d3fa282578947899c32fcff
CookieUkw <65344982+cookieukw@users.noreply.github.com>
2023-12-25 20:02:54 -0300

    Update [id].ts

--- src/pages/api/book/[id].ts
+++ src/pages/api/book/[id].ts
@@ -130,5 +130,12 @@ export default async function handler(
     "Vercel-CDN-Cache-Control",
     `public, s-maxage=${oneMonthInSeconds}`
   );
+   res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   res.status(200).json(bookInfo);
 }
