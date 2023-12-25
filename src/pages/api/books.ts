187c0993df5a359548d2f32ec5e2303feb52570d
CookieUkw <65344982+cookieukw@users.noreply.github.com>
2023-12-25 20:02:32 -0300

    Update books.ts

--- src/pages/api/books.ts
+++ src/pages/api/books.ts
@@ -5,6 +5,13 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   if (req.method === "GET") {
     await getBooksInfo(req, res);
   } else {
