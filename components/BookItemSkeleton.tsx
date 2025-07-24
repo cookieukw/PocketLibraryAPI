f80ee84ca25210507741afa38e502f608892b650
cookieukw <cookieukw@users.noreply.github.com>
2025-07-23 22:05:36 -0300

    feat: add bookitem skeleton loading

---
+++ components/BookItemSkeleton.tsx
@@ -0,0 +1,44 @@
+// src/components/BookItemSkeleton.tsx
+
+import {
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Skeleton,
+  Box,
+} from "@mui/material";
+import { motion } from "framer-motion";
+
+const BookItemSkeleton: React.FC = () => {
+  return (
+    // Animação sutil de fade-in para uma entrada mais suave
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <ListItem sx={{ my: 1 }}>
+        <ListItemIcon sx={{ mr: 2 }}>
+          {/* Skeleton para a capa do livro */}
+          <Skeleton variant="rectangular" width={60} height={85} />
+        </ListItemIcon>
+        <ListItemText
+          primary={
+            // Skeleton para o título
+            <Skeleton variant="text" sx={{ fontSize: "1rem" }} width="80%" />
+          }
+          secondary={
+            <>
+              {/* Skeleton para o autor */}
+              <Skeleton variant="text" width="50%" />
+              {/* Skeleton para informações adicionais (tamanho, formato) */}
+              <Skeleton variant="text" width="60%" />
+            </>
+          }
+        />
+      </ListItem>
+    </motion.div>
+  );
+};
+
+export default BookItemSkeleton;
